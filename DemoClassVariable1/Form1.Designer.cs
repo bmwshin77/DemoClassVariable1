c882070b66996153b395f6ac4e350ce007f81b3f
Brian Shin <bmwshin@gmail.com>
2019-10-12 16:18:42 -0700

    Add project files.

---
+++ DemoClassVariable1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace DemoClassVariable1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.firstNumTextBox = new System.Windows.Forms.TextBox();
+            this.secondNumTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.totalCountLabel = new System.Windows.Forms.Label();
+            this.overallTotalLabel = new System.Windows.Forms.Label();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // firstNumTextBox
+            // 
+            this.firstNumTextBox.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.firstNumTextBox.Location = new System.Drawing.Point(501, 103);
+            this.firstNumTextBox.Name = "firstNumTextBox";
+            this.firstNumTextBox.Size = new System.Drawing.Size(133, 22);
+            this.firstNumTextBox.TabIndex = 0;
+            this.firstNumTextBox.TextChanged += new System.EventHandler(this.firstNumTextBox_TextChanged);
+            // 
+            // secondNumTextBox
+            // 
+            this.secondNumTextBox.Location = new System.Drawing.Point(501, 146);
+            this.secondNumTextBox.Name = "secondNumTextBox";
+            this.secondNumTextBox.Size = new System.Drawing.Size(133, 22);
+            this.secondNumTextBox.TabIndex = 4;
+            this.secondNumTextBox.TextChanged += new System.EventHandler(this.secondNumTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(359, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter First Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Enter Second Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(414, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Total:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(314, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Total Number of Additions";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(397, 268);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Overall Total";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(317, 333);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 11;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(428, 333);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 12;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(538, 333);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 13;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.button3_Click);
+            this.exitButton.MouseLeave += new System.EventHandler(this.button3_Leave);
+            this.exitButton.MouseHover += new System.EventHandler(this.button3_Hover);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(501, 187);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 14;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Location = new System.Drawing.Point(501, 187);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(103, 17);
+            this.labelTotal.TabIndex = 15;
+            this.labelTotal.Text = "total goes here";
+            // 
+            // totalCountLabel
+            // 
+            this.totalCountLabel.AutoSize = true;
+            this.totalCountLabel.Location = new System.Drawing.Point(501, 224);
+            this.totalCountLabel.Name = "totalCountLabel";
+            this.totalCountLabel.Size = new System.Drawing.Size(157, 17);
+            this.totalCountLabel.TabIndex = 16;
+            this.totalCountLabel.Text = "total addition goes here";
+            this.totalCountLabel.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // overallTotalLabel
+            // 
+            this.overallTotalLabel.AutoSize = true;
+            this.overallTotalLabel.Location = new System.Drawing.Point(501, 268);
+            this.overallTotalLabel.Name = "overallTotalLabel";
+            this.overallTotalLabel.Size = new System.Drawing.Size(149, 17);
+            this.overallTotalLabel.TabIndex = 17;
+            this.overallTotalLabel.Text = "overall total goes here";
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.overallTotalLabel);
+            this.Controls.Add(this.totalCountLabel);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondNumTextBox);
+            this.Controls.Add(this.firstNumTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNumTextBox;
+        private System.Windows.Forms.TextBox secondNumTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.Label totalCountLabel;
+        private System.Windows.Forms.Label overallTotalLabel;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
---
+++ DemoClassVariable1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace DemoClassVariable1
+{
+    public partial class Form1 : Form
+    {
+
+        private double overallTotalDouble;
+        private int totalAdditions;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Hover(object sender, EventArgs e)
+        {
+            exitButton.BackColor = Color.Red;
+        }
+
+        private void button3_Leave(object sender, EventArgs e)
+        {
+            exitButton.BackColor = DefaultBackColor;
+        }
+
+        private void firstNumTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void secondNumTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //local variable declaration
+
+            double firstNumberDouble = 0;
+            double secondNumberDouble = 0;
+            double totalDouble;
+
+            if (!Regex.IsMatch(firstNumTextBox.Text, @"[0-9]") || !Regex.IsMatch(secondNumTextBox.Text, @"[0-9]"))
+            {
+                MessageBox.Show("Please enter a number");
+                firstNumTextBox.Text = "0";
+                secondNumTextBox.Text = "0";
+                firstNumTextBox.Focus();
+            }
+
+                             
+            // Use Convert Class Method
+            firstNumberDouble = Convert.ToDouble(firstNumTextBox.Text);
+            // Use Parse 
+            secondNumberDouble = double.Parse(secondNumTextBox.Text);
+
+            totalDouble = firstNumberDouble + secondNumberDouble;
+
+            labelTotal.Text = Convert.ToString(totalDouble);
+
+            totalAdditions++; // or totalAdditions +=1;
+            totalCountLabel.Text = Convert.ToString(totalAdditions);
+
+            overallTotalDouble += totalDouble;
+            overallTotalLabel.Text = overallTotalDouble.ToString ();
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            firstNumTextBox.Text = "";
+            firstNumTextBox.ForeColor = Color.Blue;
+            secondNumTextBox.Text = "";
+            secondNumTextBox.ForeColor = Color.Red;
+            labelTotal.Text = "";
+            totalCountLabel.Text = "";
+            totalAdditions = 0;
+            overallTotalLabel.Text = "";
+            overallTotalDouble = 0;
+        }
+
+        //private void firstNumTextBox_Validating(object sender, CancelEventArgs e)
+        //{
+        //    if (string.IsNullOrEmpty(firstNumTextBox.Text))
+        //    {
+        //        e.Cancel = true;
+        //        firstNumTextBox.Focus();
+        //        errorProvider.SetError(firstNumTextBox, "Please enter a valid number");
+        //    }
+
+        //    else
+        //    {
+        //        e.Cancel = true;
+        //        errorProvider.SetError(firstNumTextBox, null);
+        //    }
+        //}
+    }
+
+    //TODO 
+//Currently, you have a textbox that has some text in it.
+
+//You are to change the behavior such that when a user clicks on the text box,
+
+//The textbox clears
+//Textbox fore color changes to black or blue(more visible)
+//Exit Button behavior:
+
+//when the mouse hovers over the exit button, color should change to Red
+//when the mouse leaves the exit button, color should change back to default
+}
